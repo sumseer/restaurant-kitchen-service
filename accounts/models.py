64afd42b1d74de6bbd30e7340c18ddb977a52ab4
Max Savenya <savenya2013@gmail.com>
2024-12-03 12:36:22 +0200

    create accounts and kitchen models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Cook(AbstractUser):
+    years_of_experience = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ("username",)
+
+    def __str__(self):
+        return self.username
