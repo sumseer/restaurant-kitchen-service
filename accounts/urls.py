b8817a832909c4ed446318f585a2bc49275a08c0
Max Savenya <savenya2013@gmail.com>
2024-12-03 21:16:54 +0200

    update urls for both apps

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,14 @@ from django.urls import path, include
 from accounts import views
 
 
-urlapatterns = [
-    path("", views.index, name="index"),
+urlpatterns = [
+    path("", include("django.contrib.auth.urls")),
+    path("cooks/", views.CookListView.as_view(), name="cook-list"),
+    path("<int:pk>/", views.CookDetailView.as_view(), name="cook-detail"),
+    path("cooks/create", views.CookCreateView.as_view(), name="cook-create"),
+    path("cooks/<int:pk>/update", views.CookUpdateView.as_view(), name="cook-create"),
+    path("cooks/<int:pk>/delete", views.CookDeleteView.as_view(), name="cook-delete"),
+
 ]
 
 
