9db677555e85ab5c77026d5367ad862f871a30b1
Max Savenya <savenya2013@gmail.com>
2024-12-04 11:53:15 +0200

    update accounts views

--- accounts/views.py
+++ accounts/views.py
@@ -7,32 +7,32 @@ from accounts.models import Cook
 
 class CookListView(generic.ListView):
     model = Cook
-    template_name = "kitchen/cook_list.html"
+    template_name = "accounts/cook_list.html"
     context_object_name = "cooks"
     paginate_by = 5
 
 
 class CookDetailView(generic.DetailView):
     model = Cook
-    template_name = "kitchen/cook_detail.html"
+    template_name = "accounts/cook_detail.html"
     context_object_name = "cook"
 
 
 class CookCreateView(generic.CreateView):
     model = Cook
-    template_name = "kitchen/cook_form.html"
+    template_name = "accounts/cook_form.html"
     fields = "__all__"
-    success_url = reverse_lazy("kitchen:cook-list")
+    success_url = reverse_lazy("accounts:cook-list")
 
 
 class CookUpdateView(generic.UpdateView):
     model = Cook
-    template_name = "kitchen/cook_form.html"
+    template_name = "accounts/cook_form.html"
     fields = "__all__"
-    success_url = reverse_lazy("kitchen:cook-list")
+    success_url = reverse_lazy("accounts:cook-list")
 
 
 class CookDeleteView(generic.DeleteView):
     model = Cook
-    template_name = "kitchen/cook_confirm_delete.html"
-    success_url = reverse_lazy("kitchen:cook-list")
+    template_name = "accounts/cook_confirm_delete.html"
+    success_url = reverse_lazy("accounts:cook-list")
