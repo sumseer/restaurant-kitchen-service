07690134825a7a13ee357e1afb73fcd8f8e4acac
Max Savenya <savenya2013@gmail.com>
2024-12-07 00:30:42 +0200

    add build.sh, update prod settings

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install -r requirements.txt
+
+# Convert statis asset files
+python manage.py collectstatis --no-input
+
+# Apply any outstanding migrations
+python manage.py migrate
