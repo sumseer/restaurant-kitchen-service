78c9643cb92d9c22344455b179dbafa8465d1c1a
Max Savenya <savenya2013@gmail.com>
2024-12-07 00:32:47 +0200

    fix collectstatic typo

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Convert statis asset files
-python manage.py collectstatis --no-input
+python manage.py collectstatic --no-input
 
 # Apply any outstanding migrations
 python manage.py migrate
